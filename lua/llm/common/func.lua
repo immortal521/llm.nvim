915b7ae4d49f06e804d1cac3115b9a9c4201596d
immort <immort521@gmail.com>
2024-11-28 11:29:14 +0800

    update

--- lua/llm/common/func.lua
+++ lua/llm/common/func.lua
@@ -246,7 +246,7 @@ function M.CloseLLM()
       local _filename =
         utf8_sub(state.session[state.session.filename][2].content, 1, conf.configs.max_history_name_length)
       filename = (
-        string.format("%s%s-%s.json", dirname, _filename, os.date("%Y%m%d%H%M%S")):gsub(".", {
+        string.format("%s/%s-%s.json", dirname, _filename, os.date("%Y%m%d%H%M%S")):gsub(".", {
           ["["] = "\\[",
           ["]"] = "\\]",
           ["\n"] = " ",
